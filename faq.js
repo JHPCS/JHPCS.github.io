18a246fbff74ca00ad825ea4a27491bb68fab705
JHPCS <144303344+JHPCS@users.noreply.github.com>
2023-09-23 12:57:36 -0400

    Update faq.js

--- faq.js
+++ faq.js
@@ -1,24 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const faqAnswers = document.querySelectorAll(".faq-answer");
-
-  // Hide all FAQ answers initially
-  faqAnswers.forEach(function (answer) {
-    answer.style.display = "none";
-  });
-
   const faqQuestions = document.querySelectorAll(".faq-question");
 
   faqQuestions.forEach(function (question) {
     question.addEventListener("click", function () {
       const answer = this.nextElementSibling;
 
-      if (answer.style.display === "block" || answer.style.display === "") {
+      if (answer.style.display === "block") {
         answer.style.display = "none";
       } else {
         answer.style.display = "block";
       }
     });
   });
+});
 
 const menuIcon = document.querySelector(".menu-icon");
   const menuLinks = document.querySelector(".menu-links");
