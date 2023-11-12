a5a9d7b6f44169831af9762954758c440eb074da
JHPCS <144303344+JHPCS@users.noreply.github.com>
2023-11-12 13:53:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -317,41 +317,47 @@ document.getElementById("applySettings").addEventListener("click", function() {
 const apiKey = 'AIzaSyC80a9hJHBehf8pum1EM8YOL3_9OO3ISAc';
 
     
-    function searchVideo() {
+    function searchVideos() {
       const searchInput = document.getElementById('searchInput').value;
       if (!searchInput) {
         alert('Please enter a song name');
         return;
       }
 
-      
+     
       fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchInput}&type=video&key=${apiKey}`)
         .then(response => response.json())
         .then(data => {
           if (data.items.length === 0) {
             alert('No videos found');
           } else {
-           
-            const videoId = data.items[0].id.videoId;
-            embedPlayer(videoId);
+            
+            const videoListDiv = document.getElementById('videoList');
+            videoListDiv.innerHTML = '';
+            data.items.forEach(item => {
+              const videoTitle = item.snippet.title;
+              const videoId = item.id.videoId;
+              const listItem = document.createElement('div');
+              listItem.innerHTML = `<a href="#" onclick="playVideo('${videoId}')">${videoTitle}</a>`;
+              videoListDiv.appendChild(listItem);
+            });
           }
         })
         .catch(error => console.error('Error:', error));
     }
 
     
-    function embedPlayer(videoId) {
+    function playVideo(videoId) {
       const playerDiv = document.getElementById('player');
       playerDiv.innerHTML = ''; 
 
-      
+     
       const iframe = document.createElement('iframe');
       iframe.width = '100%';
       iframe.height = '100%';
-      iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+      iframe.src = `https://www.youtube.com/embed/${videoId}`;
       iframe.frameBorder = 0;
       iframe.allowFullscreen = true;
       playerDiv.appendChild(iframe);
     }
-
 
