bee6f07b369fc402007bcfd113cd16a1bc86f415
JHPCS <144303344+JHPCS@users.noreply.github.com>
2023-11-16 16:55:20 -0500

    Update dono.js

--- dono.js
+++ dono.js
@@ -1,9 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-  
-    const menuIcon = document.querySelector(".menu-icon");
-    const menuLinks = document.querySelector(".menu-links");
+const menuIcon = document.querySelector(".menu-icon");
+  const menuLinks = document.querySelector(".menu-links");
 
-    menuIcon.addEventListener("click", () => {
-        menuLinks.classList.toggle("open");
-    });
-});
+  menuIcon.addEventListener("click", () => {
+      menuLinks.classList.toggle("show-menu");
+  });
+;
+function toggleMenu() {
+  const menuLinks = document.querySelector(".menu-links");
+  menuLinks.classList.toggle("open"); 
+}
