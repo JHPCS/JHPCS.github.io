4db68f0ed14f3d972119f877a2c2b9c3d2d3e3fe
JHPCS <144303344+JHPCS@users.noreply.github.com>
2023-11-16 16:09:29 -0500

    Update WD.js

--- WD.js
+++ WD.js
@@ -9,3 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+const menuIcon = document.querySelector(".menu-icon");
+  const menuLinks = document.querySelector(".menu-links");
+
+  menuIcon.addEventListener("click", () => {
+      menuLinks.classList.toggle("show-menu");
+  });
+;
+function toggleMenu() {
+  const menuLinks = document.querySelector(".menu-links");
+  menuLinks.classList.toggle("open"); 
+}
