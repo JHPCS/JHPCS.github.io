d43f773a3cf9067375e22b1d39501ad6c4b21e39
JHPCS <144303344+JHPCS@users.noreply.github.com>
2023-12-15 08:59:30 -0500

    Update process_contact_form.php

--- process_contact_form.php
+++ process_contact_form.php
@@ -5,27 +5,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    // Set recipient email address
-    $to = "jmawmaw711@gmail.com"; // Replace with your email address
+    
+    $to = "jmawmaw711@gmail.com"; 
 
-    // Set email subject
+    
     $subject = "Contact Form Submission from $name";
 
-    // Build email message
+    
     $email_message = "Name: $name\n";
     $email_message .= "Email: $email\n";
     $email_message .= "Message:\n$message";
 
-    // Send email
+    
     if (mail($to, $subject, $email_message)) {
-        // Email sent successfully
+        
         echo "Thank you for contacting us. We will get back to you soon.";
     } else {
-        // Email sending failed
+        
         echo "Sorry, there was an error sending your message.";
     }
 } else {
-    // If the form was not submitted, display an error message
+     
     echo "Something went wrong. Please try again later.";
 }
 ?>
