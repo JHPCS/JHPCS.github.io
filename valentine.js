43fa0dcfd894b12c096f9b746a766e00c6515abe
JHPCS <144303344+JHPCS@users.noreply.github.com>
2024-02-06 19:45:50 -0500

    Update valentine.js

--- valentine.js
+++ valentine.js
@@ -1,17 +1,1 @@
-// Add event listener to answer submission button
-const button = document.querySelector('button');
-button.addEventListener('click', function(event) {
-  event.preventDefault();
 
-  // Get user input
-  const answer = document.querySelector('input[name="answer"]').value;
-
-  // Check if answer is correct
-  if (answer.toLowerCase() === "your answer goes here") {
-    // Show success message
-    alert("Congratulations! You guessed the riddle correctly!");
-  } else {
-    // Show error message
-    alert("Oops, that's not quite right. Try again!");
-  }
-});
